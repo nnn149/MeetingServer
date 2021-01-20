e8dc2a639c646f2bd56d427963b161147df1f3cf
Nannan <1041836312@qq.com>
2021-01-21 02:05:03 +0800

    修改ip
    修改ip

--- src/main/java/cn/nicenan/meeting/controller/WebrtcWsController.java
+++ src/main/java/cn/nicenan/meeting/controller/WebrtcWsController.java
@@ -34,7 +34,7 @@ public class WebrtcWsController {
     @GetMapping("/url")
     public JsonResult getIpAddress() {
         //http用ws,https用wss,ws可以用ip访问，wss的话，用ip会报证书错误，需要用域名访问,
-        // return new JsonResult("ws:192.168.2.203:" + port + "/ws/webrtc");
-        return new JsonResult("wss:192.168.2.203:" + port + "/ws/webrtc");
+        // return new JsonResult("ws:192.168.2.108:" + port + "/ws/webrtc");
+        return new JsonResult("wss:192.168.2.108:" + port + "/ws/webrtc");
     }
 }
